a26dde26f35b3a0928bc868a9fa18365e8fe806c
Vasil Tsvetanov <vask_o@abv.bg>
2023-02-03 14:46:27 +0200

    Improvements for ActionPanel ui

--- src/components/ui/ActionPanel.tsx
+++ src/components/ui/ActionPanel.tsx
@@ -1,11 +1,65 @@
 import React from 'react';
+import Slider from 'rc-slider';
+import 'rc-slider/assets/index.css';
+
+import {
+  PenTool as PenToolIcon,
+  Square as SquareIcon,
+  Circle as CircleIcon,
+  ArrowLeft as ArrowLeftIcon,
+  ArrowRight as ArrowRightIcon,
+  ArrowUp as ArrowUpIcon,
+  CornerDownLeft as CornerDownLeftIcon,
+  CornerDownRight as CornerDownRightIcon,
+} from 'react-feather';
 
 interface ActionPanelProps {}
 
-const ActionPanel: React.FC<ActionPanelProps> = () => (
-  <header className="h-40 p-10 bg-white decoration-orange-400 shadow-md border-b">
-    Some content
-  </header>
-);
+const ActionPanel: React.FC<ActionPanelProps> = () => {
+  const handleSlider = (sliderValue: number | number[]) => {
+    console.log({ sliderValue });
+  };
+
+  return (
+    <header className="h-80 p-10 flex flex-row gap-3 bg-white shadow-md">
+      {/* Draw Instruments */}
+      <div className="h-60 w-60 bg-gray-200 flex flex-row flex-wrap justify-center gap-3">
+        <span className="flex items-center max-h-8 m-2 p-3 bg-white hover:bg-gray-100 cursor-pointer">
+          <PenToolIcon />
+        </span>
+        <span className="flex items-center max-h-8 m-2 p-3 bg-white hover:bg-gray-100 cursor-pointer">
+          <SquareIcon />
+        </span>
+        <span className="flex items-center max-h-8 m-2 p-3 bg-white hover:bg-gray-100 cursor-pointer">
+          <CircleIcon />
+        </span>
+        <span className="flex items-center max-h-8 m-2 p-3 bg-white hover:bg-gray-100 cursor-pointer">
+          <ArrowLeftIcon />
+        </span>
+        <span className="flex items-center max-h-8 m-2 p-3 bg-white hover:bg-gray-100 cursor-pointer">
+          <ArrowRightIcon />
+        </span>
+        <span className="flex items-center max-h-8 m-2 p-3 bg-white hover:bg-gray-100 cursor-pointer">
+          <ArrowUpIcon />
+        </span>
+      </div>
+      {/* Slider */}
+      <div className="h-60 w-60 bg-gray-200 flex flex-row flex-wrap justify-center gap-3">
+        <span className="flex items-center w-full max-h-10 m-2 p-3 bg-white">
+          <Slider defaultValue={50} onChange={handleSlider} />
+        </span>
+      </div>
+      {/* Undo/Redo */}
+      <div className="h-20 w-40 bg-gray-200 flex flex-row flex-wrap justify-center items-center	gap-3">
+        <span className="flex items-center max-h-8 m-2 p-3 bg-white hover:bg-gray-100 cursor-pointer">
+          <CornerDownLeftIcon />
+        </span>
+        <span className="flex items-center max-h-8 m-2 p-3 bg-white hover:bg-gray-100 cursor-pointer">
+          <CornerDownRightIcon />
+        </span>
+      </div>
+    </header>
+  );
+};
 
 export default ActionPanel;
