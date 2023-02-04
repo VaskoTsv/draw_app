5d436169102553cd35e69a59ff1bdf869a3e736e
Vasil Tsvetanov <vask_o@abv.bg>
2023-02-04 12:36:35 +0200

    Move draw logic to store

--- src/components/ui/Canvas.tsx
+++ src/components/ui/Canvas.tsx
@@ -1,74 +1,28 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useRef } from 'react';
+
+import { observer } from 'mobx-react-lite';
+import appStore from '~/store';
 
 interface CanvasProps {}
 
-const Canvas: React.FC<CanvasProps> = () => {
-  const [isDrawing, setIsDrawing] = useState(false);
-
+const Canvas: React.FC<CanvasProps> = observer(() => {
   const canvasRef = useRef<HTMLCanvasElement | null>(null);
-  const contextRef = useRef<CanvasRenderingContext2D | null>(null);
 
   useEffect(() => {
-    const canvas = canvasRef.current as HTMLCanvasElement;
-    canvas.width = window.innerWidth * 2;
-    canvas.height = window.innerHeight * 2;
-    // canvas.style.width = `${window.innerWidth}px`;
-    // canvas.style.height = `${window.innerHeight}px`;
-
-    const context = canvas.getContext('2d') as CanvasRenderingContext2D;
-    context.scale(2, 2);
-    context.lineCap = 'round';
-    context.strokeStyle = 'black';
-    context.lineWidth = 1;
-
-    contextRef.current = context;
+    appStore.drawStore.setCanvas(canvasRef.current as HTMLCanvasElement);
   }, []);
-
-  const startDrawing = ({ nativeEvent }: React.MouseEvent) => {
-    const { offsetX, offsetY } = nativeEvent;
-    contextRef?.current?.beginPath();
-    contextRef.current?.moveTo(offsetX, offsetY);
-    setIsDrawing(true);
-  };
-
-  const finishDrawing = () => {
-    contextRef?.current?.closePath();
-    setIsDrawing(false);
-  };
-
-  const draw = ({ nativeEvent }: React.MouseEvent) => {
-    if (!isDrawing) {
-      return;
-    }
-    const { offsetX, offsetY } = nativeEvent;
-    contextRef?.current?.lineTo(offsetX, offsetY);
-    contextRef?.current?.stroke();
-  };
-
-  const clearCanvas = () => {
-    const canvas = canvasRef.current;
-    const context = canvas?.getContext('2d');
-
-    if (!canvas || !context) {
-      return;
-    }
-
-    context.fillStyle = 'white';
-    context.fillRect(0, 0, canvas.width, canvas.height);
-  };
-  console.log(clearCanvas);
 
   return (
     <div className="flex items-center justify-center m-2 border-1 border-gray-400">
       <canvas
         className="h-full w-full bg-white shadow-md"
         ref={canvasRef}
-        onMouseDown={startDrawing}
-        onMouseUp={finishDrawing}
-        onMouseMove={draw}
+        onMouseDown={appStore.drawStore.startDrawing}
+        onMouseUp={appStore.drawStore.finishDrawing}
+        onMouseMove={appStore.drawStore.draw}
       />
     </div>
   );
-};
+});
 
 export default Canvas;
--- src/store/draw.store.ts
+++ src/store/draw.store.ts
@@ -3,14 +3,23 @@ import { makeAutoObservable, observable } from 'mobx';
 import DrawInstrumentsEnum from '~/features/draw/interfaces/instruments.interface';
 
 class DrawStore {
+  canvas: HTMLCanvasElement | null = null;
+
+  context: CanvasRenderingContext2D | null = null;
+
+  isDrawing: boolean = false;
+
   drawInstrument = DrawInstrumentsEnum.Pen;
 
-  strokeWidth = 50;
+  strokeWidth = 1;
 
   constructor() {
     makeAutoObservable(
       this,
       {
+        canvas: observable,
+        context: observable,
+        isDrawing: observable,
         drawInstrument: observable,
         strokeWidth: observable,
       },
@@ -27,6 +36,48 @@ class DrawStore {
 
   setStrokeWidth(width: number | number[]) {
     this.strokeWidth = width as number;
+  }
+
+  setCanvas(canvasElement: HTMLCanvasElement) {
+    this.canvas = canvasElement;
+    this.canvas.width = window.innerWidth * 2;
+    this.canvas.height = window.innerHeight * 2;
+
+    this.context = this.canvas.getContext('2d') as CanvasRenderingContext2D;
+    this.context.scale(2, 2);
+    this.context.lineCap = 'round';
+    this.context.strokeStyle = 'black';
+    this.context.lineWidth = this.strokeWidth;
+  }
+
+  startDrawing({ nativeEvent }: React.MouseEvent) {
+    const { offsetX, offsetY } = nativeEvent;
+    this.context?.beginPath();
+    this.context?.moveTo(offsetX, offsetY);
+    this.isDrawing = true;
+  }
+
+  finishDrawing() {
+    this.context?.closePath();
+    this.isDrawing = false;
+  }
+
+  draw({ nativeEvent }: React.MouseEvent) {
+    if (!this.isDrawing) {
+      return;
+    }
+    const { offsetX, offsetY } = nativeEvent;
+    this.context?.lineTo(offsetX, offsetY);
+    this.context?.stroke();
+  }
+
+  clearCanvas() {
+    if (!this.canvas || !this.context) {
+      return;
+    }
+
+    this.context.fillStyle = 'white';
+    this.context.fillRect(0, 0, this.canvas.width, this.canvas.height);
   }
 }
 
